b83bbd6f46b8e3ab97439dc7a1391992c748337b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-21 16:16:51 +0200

    first commit of programs model, admin.py and models.py created

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Programs
 
 # Register your models here.
+
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ( 
+                'name',
+                'type',
+                'description',
+                'price',
+    )
+
+admin.site.register(Programs, ProgramAdmin)
--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Programs(models.Model):
+    """
+    model for different programs on sale
+    """
+    name = models.CharField(max_length=100)
+    type = models.DecimalField(max_digits=2, decimal_places=0)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name 
