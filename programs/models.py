4410cf9e491d3ecadae2e00ce20b88f93990e489
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-21 16:18:57 +0200

    plural spelling of programs model fixed in admin view

--- programs/models.py
+++ programs/models.py
@@ -6,6 +6,10 @@ class Programs(models.Model):
     """
     model for different programs on sale
     """
+
+    class Meta: 
+        verbose_name_plural = 'Programs'
+
     name = models.CharField(max_length=100)
     type = models.DecimalField(max_digits=2, decimal_places=0)
     description = models.TextField()
