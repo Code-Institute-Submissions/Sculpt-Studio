15e79ef51c32302c6c522d799c4aefbf0bd5dac0
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-21 11:44:11 +0200

    set up of about me page under home app, copied content from index and modified. urls + views done. base html link fixed for home / about

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     renders index.html
     """
     return render(request, 'home/index.html')
+
+def about_me(request):
+    """
+    renders about-me.html
+    """
+    return render(request, 'home/about-me.html')
