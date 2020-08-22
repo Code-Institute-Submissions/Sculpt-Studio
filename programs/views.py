720bc64c3da13c294598b4efe676d43f314982ec
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-22 13:07:02 +0200

    edit_program function working, links working

--- programs/views.py
+++ programs/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Programs
 from .forms import ProgramForm
 
-# Create your views here.
+
 
 def programs(request):
     """
@@ -37,8 +37,17 @@ def edit_program(request, program_id):
     """
     programs = get_object_or_404(Programs, pk=program_id)
 
+    if request.method == 'POST':
+        form = ProgramForm(request.POST, instance=programs)
+        if form.is_valid:
+            form.save()
+            return redirect(reverse('program_details', args=[programs.id]))
+    else:
+        form = ProgramForm(instance=programs)
+
     context = {
-        'programs': programs
+        'programs': programs,
+        'form': form
     }
 
     return render(request, 'programs/edit_program.html', context)
