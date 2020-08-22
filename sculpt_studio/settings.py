8124f1943a099ca2e440c12dc274e1145857933b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-22 12:16:01 +0200

    updated settings.py with crispy forms app

--- sculpt_studio/settings.py
+++ sculpt_studio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispt_forms',
     'home',
     'programs'
 ]
@@ -157,3 +158,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
