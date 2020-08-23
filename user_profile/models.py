4d6d659e88a5c3a1d57c271fe923764d3c809ced
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-23 20:18:06 +0200

    installed django cuntries + started UserProfile model for user_profile app

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=264, blank=True )
+    country = CountryField()
+    
+
