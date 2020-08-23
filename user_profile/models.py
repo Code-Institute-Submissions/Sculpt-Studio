508adc3b4dc044348bfcd414823e64a1f6ce2855
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-23 20:20:24 +0200

    django_countries installed

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=264, blank=True )
-    country = CountryField()
-    
+    address = models.CharField(max_length=264, blank=True)
+    country = CountryField(blank=True)
 
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
