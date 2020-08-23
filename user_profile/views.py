1fc3d714c12f704ce9302ac108a592b783900ff7
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-23 21:47:17 +0200

    started work on adding form to account landing page where user can update details + forms.py created

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.contrib.auth.decorators import login_required
+from .forms import UserForm
 
 # Create your views here.
 
@@ -7,4 +8,16 @@ from django.contrib.auth.decorators import login_required
 def account(request):
     """render my_profile.html"""
 
-    return render(request, 'user_profile/my_account.html')
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect(reverse('account'))
+    else:
+        user_form = UserForm(instance=request.user)
+
+    context = {
+        'user_form': user_form
+    }
+
+    return render(request, 'user_profile/my_account.html', context)
