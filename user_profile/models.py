9014fba540011508f0f5e5f067c8a65c2a16e444
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-23 20:49:38 +0200

    UserProfile model working and tested

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,7 +4,7 @@ from django_countries.fields import CountryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=264, blank=True)
     country = CountryField(blank=True)
@@ -13,7 +13,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
