0f0226eb3c0ca402d65bf531558ca0dc7600667e
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-25 13:02:44 +0200

    def account / UserProfile post error fixed - added get:_object in account for user_profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render,redirect,reverse
+from django.shortcuts import render,redirect,reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm, UserManagementForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 @login_required
 def account(request):
     """render my_profile.html and handle profile update """
+    user_profile = get_object_or_404(Profile, user=request.user)
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
+        user_form = UserForm(request.POST, instance=user_profile)
         if user_form.is_valid():
             user_form.save()
             messages.success(request, f'Profile has been updated!')
-            return redirect(reverse('account'))
     else:
         user_form = UserForm(instance=request.user)
 
@@ -32,7 +33,6 @@ def user_management(request):
             form = UserManagementForm(request.POST, instance=request.user)
             if form.is_valid():
                 form.save()
-                return redirect(reverse('account'))
         else:
             form = UserManagementForm(instance=request.user)
     else:
