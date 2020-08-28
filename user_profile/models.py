095b9bcbb299b9d5c4b5b2576d08fdd61f0177ac
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-28 13:05:31 +0200

    first draft of Testimonials Model class started

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from programs.models import Programs
+from django.core.validators import MaxValueValidator, MinValueValidator 
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -24,3 +27,11 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Testimonials(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    program = models.ForeignKey(Programs, on_delete=models.CASCADE)
+    review = models.TextField(max_length=2064)
+    score = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    date = models.DateField()
