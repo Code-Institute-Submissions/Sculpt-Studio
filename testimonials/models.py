d0414cdbcb44f9fa54aca0b728f2fcb074d3b71b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-30 19:13:58 +0200

    testimonials --> urls.py, views.py , templates folder, template, models + migration, project level urls.py and base.html link updated

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
+from programs.models import Programs
+from user_profile.models import Profile
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Testimonials(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    review = models.TextField(max_length=2064)
+    score = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    date = models.DateField()
