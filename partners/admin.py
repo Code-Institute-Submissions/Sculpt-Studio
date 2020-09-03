58a773e6166a4f6e41989824947728ea7e9971b0
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-03 15:22:40 +0200

    PartnersAdmin class to admin.py in partners app

--- partners/admin.py
+++ partners/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from .models import Partners
 
 
-class TestimonialsAdmin(admin.ModelAdmin):
+class PartnersAdmin(admin.ModelAdmin):
     list_display = ( 
-        'name'
-        'summary'
-        'main_contact'
-        'main_contact_phone'
-        'main_contact_email'
-        'discount'
-        'link'
-        'deal_validity'
+        'name',
+        'summary',
+        'main_contact',
+        'main_contact_phone',
+        'main_contact_email',
+        'discount',
+        'link',
+        'deal_validity',
     )
 
 admin.site.register(Partners, PartnersAdmin)
