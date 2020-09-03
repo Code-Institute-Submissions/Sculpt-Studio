67e14aa1e960cea984dbf61693b86de700f87a95
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-03 15:00:09 +0200

    base.html link update with partners link + initial partners.html started urls and views initiated

--- partners/views.py
+++ partners/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def partners(request):
+    '''all partners view'''
+
+    return render(request, 'partners/partners.html')
