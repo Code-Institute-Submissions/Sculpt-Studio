ae88e33fcfb30f14a15b89dd50dfbba1e2053283
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-03 10:41:01 +0200

    delete testimonials added for superuser on testimonials page

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.testimonials, name='testimonials'),
     path('add_testimonials/', views.add_testimonials, name='add_testimonials'),
+    path('delete_testimonials/<int:testimonial_id>', views.delete_testimonials, name='delete_testimonials'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -38,3 +38,17 @@ def add_testimonials(request):
 
     return render(request, 'testimonials/add_testimonials.html', context) 
 
+
+@login_required
+def delete_testimonials(request, testimonial_id):
+    '''allow admin user to delete inappropriate testimonials'''
+    testimonial = get_object_or_404(Testimonials, pk=testimonial_id)
+
+    if not request.user.is_superuser:
+        messages.error(request, f'You must be and administrative user to use this function')
+        return redirect(reverse('home'))
+    
+    testimonial.delete()
+
+    return redirect(reverse('testimonials'))
+
