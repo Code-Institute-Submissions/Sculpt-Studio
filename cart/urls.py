ed9e626cf8405aa99587b04ec97e0ae649232b5c
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-05 20:14:41 +0200

    cart app removed

--- cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.cart, name='cart'),
-    path('add/<program_id>', views.add_to_cart, name='add_to_cart'),
-]
