0cc8a70bc65af2c4bc819672b0ce2c57b03e1d3b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-05 18:45:42 +0200

    started with models.py for purchase app + purchase_checkout.html created

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from programs.models import Programs
+from user_profile.models import Profile
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Checkout(models.Model):
+    '''
+    model for checkout page
+    '''
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    billing_address = models.CharField(max_length=256)
+    billing_postcode = models.CharField(max_length=15)
+    billing_city = models.CharField(max_length=32)
+    billing_country = CountryField()
+
+
