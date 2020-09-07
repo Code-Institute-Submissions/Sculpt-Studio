3d0c7143a87cebc0b6d715037ce018a9e3b883e2
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-07 21:28:20 +0200

    stripe payment function started again

--- purchase/views.py
+++ purchase/views.py
@@ -6,10 +6,12 @@ from user_profile.models import Profile
 from programs.models import Programs
 from django.contrib.auth.models import User
 import stripe
-import json
 from django.http import HttpResponse
 from django.conf import settings
 
+
+stripe_public_key = settings.STRIPE_PUBLIC_KEY
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 @login_required
 def purchase_checkout(request, program_id):
@@ -18,19 +20,8 @@ def purchase_checkout(request, program_id):
     '''
     profile = get_object_or_404(Profile, user=request.user)
     program = get_object_or_404(Programs, pk=program_id)
-    form = CheckoutForm()
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe.api_key = settings.STRIPE_SECRET_KEY 
+    form = CheckoutForm(request.POST) 
 
-    try: 
-        stripe.PaymentIntent.create(
-            amount = int(program.price),
-            currency = 'eur'
-        )
-
-    except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be processed!')
-        return HttpResponse(content=e, status=400)
 
     context = {
         'form': form,
--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,3 +1,15 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+
+    for below code i have used codeinstitute boutique ado videos to guide me through the process
+    while trying to keep it my own project specific
+
+*/
+
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1)
 var clientSecret = $('#id_client_secret').text().slice(1, -1)
 var stripe = Stripe(stripePublicKey);
@@ -6,45 +18,37 @@ var elements = stripe.elements();
 
 var style = {
     base: {
-        color: "#32325d",
-        fontFamily: 'Arial, sans-serif',
-        fontSmoothing: "antialiased",
-        fontSize: "16px",
-        "::placeholder": {}
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
     invalid: {
-        fontFamily: 'Arial, sans-serif',
-        color: "#fa755a",
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
 };
 
-var card = elements.create("card", { style: style });
-// Stripe injects an iframe into the DOM
-card.mount("#card-element");
+var card = elements.create('card', { style: style });
+card.mount('#card-element');
 
 
-var form = document.getElementById("payment-form");
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    // Complete payment when the submit button is clicked
-    payWithCard(stripe, card, data.clientSecret);
-});
+/* handle validation errors */
 
-var payWithCard = function(stripe, card, clientSecret) {
-    loading(true);
-    stripe
-        .confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card
-            }
-        })
-        .then(function(result) {
-            if (result.error) {
-                // Show error to your customer
-                showError(result.error.message);
-            } else {
-                // The payment succeeded!
-                orderComplete(result.paymentIntent.id);
-            }
-        });
-};
+// Show the customer the error from Stripe if their card fails to charge
+
+card.AddEvenListener('change', function(event) {
+    var errorDiv = document.getElementById('#card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-exclamation-circle"></i>
+            </span>
+            <span>${event.error.message}</span> `
+    } else {
+        errorDiv.textContent = '';
+    }
+})
