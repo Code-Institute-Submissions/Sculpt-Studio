e6c76e0cdf87d3e29c74e049293eb06cb40f1297
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-11 16:45:48 +0200

    purchase_checkout.html styled and cart content made available, urls.py updated,  views.py updated with cart content

--- purchase/urls.py
+++ purchase/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<int:program_id>', views.purchase_checkout, name='purchase_checkout'),
+    path('', views.purchase_checkout, name='purchase_checkout'),
     path('purchase_successful', views.purchase_successful, name='purchase_successful'),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -7,20 +7,21 @@ from programs.models import Programs
 from .models import Checkout
 from django.contrib.auth.models import User
 import stripe
+import json
 from django.http import HttpResponse
 from django.conf import settings
-
+from cart.contexts import cart_content
 
 
 @login_required
-def purchase_checkout(request, program_id):
+def purchase_checkout(request):
     '''
     return purchase checkout page 
     '''
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     profile = get_object_or_404(Profile, user=request.user)
-    program = get_object_or_404(Programs, pk=program_id)
+    cart = request.session.get('cart', {})
 
 
 
@@ -30,23 +31,22 @@ def purchase_checkout(request, program_id):
             form.save()
             return redirect(reverse('purchase_successful'))
     else:
-        cost = program.price
+        cart_now = cart_content(request)
+        cost = cart_now['total']
         stripe_cost = round(cost * 100)
         stripe.api_key = stripe_secret_key
-
+        cart = json.dumps(cart)
         intent = stripe.PaymentIntent.create(
-            amount=stripe_cost,
-            currency=settings.STRIPE_CURRENCY
+            amount = stripe_cost,
+            currency = settings.STRIPE_CURRENCY
+
         )
-
         form = CheckoutForm()
-
-
 
     context = {
         'form': form,
         'profile': profile,
-        'program': program,
+        'cart': cart_now,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret
     }
