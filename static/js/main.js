27a6ef88ea76146f3c7c7a76240953b09b699f11
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-11 11:34:37 +0200

    get elemenrt id and reload page added to remove class in cart + js function written for that action

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,7 @@
 //get Id for programs in cart //
+
+document.querySelector('.remove').addEventListener('click', function() {
+    let getId = document.querySelector('.remove').id.split('program_')[1];
+    let url = window.location.href + 'remove_from_cart/' + `${getId}`;
+    window.location.href = url;
+})
