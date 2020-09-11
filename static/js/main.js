12fc0e833886e928635813503471b2498ba046a8
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-11 12:10:11 +0200

    main.js onclick to remove from cart function updated to work with each onclick regardless of order

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,9 @@
 //get Id for programs in cart //
 
-document.querySelector('.remove').addEventListener('click', function() {
-    let getId = document.querySelector('.remove').id.split('program_')[1];
-    let url = window.location.href + 'remove_from_cart/' + `${getId}`;
-    window.location.href = url;
-})
+document.querySelectorAll('.remove').forEach(e => {
+    e.addEventListener('click', function() {
+        let getId = this.id.split('program_')[1];
+        let url = window.location.href + 'remove_from_cart/' + `${getId}`;
+        window.location.href = url;
+    })
+});
