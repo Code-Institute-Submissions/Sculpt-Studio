aafafbae18bde6fdb7e62e42bdc1d3681856a04e
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-18 21:36:35 +0200

    updated allowed hosts in settings.py

--- sculpt_studio/settings.py
+++ sculpt_studio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sculpt-studio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -122,9 +122,9 @@ WSGI_APPLICATION = 'sculpt_studio.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+        DATABASES = {
+            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
 else:
     DATABASES = {
         'default': {
