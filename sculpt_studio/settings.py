fe2cbf53202cc3a1879b39520682c3d39db105f0
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-18 19:40:52 +0200

    secret key removed from settings.py at moved to env.py

--- sculpt_studio/settings.py
+++ sculpt_studio/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3bej99yg_gsrk*5kq%v2hf+3+f320=#=22%c*c3j1hz&sktte8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
