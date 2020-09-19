58d04d7007675b4cc7e9b37f223852cdbef7817b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-19 11:37:15 +0200

    spell error from settings.py removed

--- sculpt_studio/settings.py
+++ sculpt_studio/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'EU (Stockholm)' 
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKETNAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     #Static and medial files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
