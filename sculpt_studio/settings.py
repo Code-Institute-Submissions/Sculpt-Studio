384dade0b9db82c18848839646383010dfa0ee80
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-19 11:59:32 +0200

    bucket name updated to remove capitalization

--- sculpt_studio/settings.py
+++ sculpt_studio/settings.py
@@ -182,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     #Bucket configuration
-    AWS_STORAGE_BUCKET_NAME = 'Sculpt-Studio'
+    AWS_STORAGE_BUCKET_NAME = 'sculpt-studio'
     AWS_S3_REGION_NAME = 'eu-north-1' 
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
