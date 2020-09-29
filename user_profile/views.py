08a9c3c0e20bd8758a0e46dfe733d1d472bbf98b
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-09-29 19:49:38 +0200

    fixed problematic import

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,7 +5,7 @@ from purchase.models import Checkout
 from django.contrib import messages
 from .models import Profile
 from django.db.models import Q
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 
 @login_required
